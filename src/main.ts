08911846f9340f67b6bd20ab498a8e401e8ce081
tana-gh <yozoranotakaku@gmail.com>
2022-08-21 09:32:59 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+const h1  = document.createElement('h1')
+h1.textContent = 'Vite + Typescript'
+app.appendChild(h1)
